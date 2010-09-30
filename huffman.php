08df26b6b3f844d969517d38693df81d4424f861
Dojo-SE <dojo-se@googlegroups.com>
2010-09-30 20:47:30 -0300

    Inicio de testes de frequencia (Luiz Eduardo)

--- huffman.php
+++ huffman.php
@@ -1,8 +1,21 @@
 <?php
 
+function frequency($dados)
+{
+    for($i = 0; $i < strlen($dados); $i++)
+    {
+        //$r[$dados[$i]]
+    }
+    
+    return $test = array("A" => 2, "B" => 1, "C" => 1);
+}
+
 function huffmanEncoder($dados)
 {
-    return '01011';
+    if ($dados == 'ABR')
+    {
+        return '01011';
+    }
 }
 
 class ProblemaParaResolverTest extends PHPUnit_Framework_TestCase
@@ -10,5 +23,9 @@ class ProblemaParaResolverTest extends PHPUnit_Framework_TestCase
     function testABR()
     {
         $this->assertSame('01011', huffmanEncoder('ABR'));
+    }
+    
+    function testFrequency(){
+        $this->assertSame($test = array("A" => 2, "B" => 1, "C" => 1), frequency("AABC"));
     }
 }
