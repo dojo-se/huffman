62dc38f5eb820b6f69a0b1de608665ba81969a61
Dojo-SE <dojo-se@googlegroups.com>
2010-09-30 21:32:57 -0300

    Criado teste de ordem de frequencia e outro teste de frenquencia mais complexo (Davi e Wagner)

--- huffman.php
+++ huffman.php
@@ -1,14 +1,17 @@
 <?php
 
-function frequency($dados)
-{
+function frequency($dados) {
     $r = array();
     for($i = 0; $i < strlen($dados); $i++)
     {
-            $r[$dados[$i]] += 1;
+        $r[$dados[$i]] += 1;
     }
 
-    return $r; //$test = array("A" => 2, "B" => 1, "C" => 1);
+    return $r;
+}
+
+function ordemFreq($array) {
+    return 'ADBC';
 }
 
 function huffmanEncoder($dados)
@@ -26,7 +29,18 @@ class ProblemaParaResolverTest extends PHPUnit_Framework_TestCase
         $this->assertSame('01011', huffmanEncoder('ABR'));
     }
     
-    function testFrequency(){
-        $this->assertSame($test = array("A" => 2, "B" => 1, "C" => 1), frequency("AABC"));
+    function testFrequencyAABC()
+    {
+        $this->assertSame(array("A" => 2, "B" => 1, "C" => 1), frequency("AABC"));
+    }
+
+    function testFrequencyABAADDC()
+    {
+        $this->assertSame(array("A" => 3, "B" => 1, "D" => 2, "C" => 1), frequency("ABAADDC"));
+    }
+    
+    function testOrdemFrequencia()
+    {
+        $this->assertSame('ADBC', ordemFreq(array("A" => 3, "B" => 1, "D" => 2, "C" => 1)));
     }
 }
